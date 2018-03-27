f635d77d72b7aa1a5c075eca9fcbfbc933188508
longbai <slongbai@gmail.com>
2018-03-27 08:47:58 +0800

    init

---
+++ simple-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <CompletedTodos></CompletedTodos>
+    <CurrentTodos></CurrentTodos>
+    <CreateTodo></CreateTodo>
+  </div>
+</template>
+
+<script>
+import CreateTodo from './components/CreateTodo'
+import CurrentTodos from './components/CurrentTodos'
+import CompletedTodos from './components/CompletedTodos'
+export default {
+  name: 'App',
+  components: {
+    CreateTodo,
+    CurrentTodos,
+    CompletedTodos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ simple-todo/src/components/CreateTodo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="get-todo" class="container">
+    <input class="form-control" :value="newTodo" placeholder="I need to...." @change="createTodo">
+    <button class="btn btn-primary" @click="addTodo">Add Todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    createTodo (e) {
+      this.$store.dispatch('createTodo', e.target.value)
+    },
+    addTodo () {
+      this.$store.dispatch('addTodo')
+      this.$store.dispatch('clearTodo')
+    }
+  },
+  computed: {
+    newTodo () {
+      return this.$store.getters.newTodo
+    }
+  }
+}
+</script>
